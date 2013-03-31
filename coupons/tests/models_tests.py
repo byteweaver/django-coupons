db239cdd4ddb32009f2b154950ab0213dcc0a6dc
akuryou <akuryou@byteweaver.net>
2013-03-31 20:39:14 +0200

    add CouponManager tests

--- coupons/tests/models_tests.py
+++ coupons/tests/models_tests.py
@@ -10,3 +10,12 @@ class CouponTestCase(TestCase):
     def test_generate_code(self):
         self.assertIsNotNone(re.match("^[%s]{%d}" % (CODE_CHARS, CODE_LENGTH,), Coupon.generate_code()))
 
+    def test_create_coupon(self):
+        coupon = Coupon.objects.create_coupon('menetary', 100)
+        self.assertTrue(coupon.pk)
+
+    def test_create_coupons(self):
+        coupons = Coupon.objects.create_coupons(50, 'menetary', 100)
+        for coupon in coupons:
+            self.assertTrue(coupon.pk)
+
