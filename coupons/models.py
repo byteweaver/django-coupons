94d42f40834e1c78570b3758988f35c60957bac0
akuryou <akuryou@byteweaver.net>
2013-03-31 20:48:22 +0200

    auto generate code if coupon is saved without code

--- coupons/models.py
+++ coupons/models.py
@@ -33,7 +33,8 @@ class CouponManager(models.Manager):
 
 class Coupon(models.Model):
     value = models.IntegerField(_("Value"), help_text=_("Arbitrary coupon value"))
-    code = models.CharField(_("Code"), max_length=30, unique=True)
+    code = models.CharField(_("Code"), max_length=30, unique=True, blank=True,
+        help_text=_("Leaving this field empty will generate a random code."))
     type = models.CharField(_("Type"), max_length=20, choices=COUPON_TYPES)
     user = models.ForeignKey(User, verbose_name=_("User"), null=True, blank=True,
         help_text=_("You may specify a user youn with to limit this coupon to"))
@@ -49,6 +50,11 @@ class Coupon(models.Model):
 
     def __unicode__(self):
         return self.code
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = Coupon.generate_code()
+        super(Coupon, self).save(*args, **kwargs)
 
     @classmethod
     def generate_code(cls):
