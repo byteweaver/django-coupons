feb54cb3a1127f5f7c1c88d24800aea3b86bd0d8
akuryou <akuryou@byteweaver.net>
2013-04-04 19:01:01 +0200

    add basic CouponFormTestCase

--- coupons/tests/forms_tests.py
+++ coupons/tests/forms_tests.py
@@ -1,11 +1,25 @@
 from django.test import TestCase
 
-from coupons.forms import CouponGenerationForm
-
+from coupons.forms import CouponGenerationForm, CouponForm
+from coupons.models import Coupon
 
 class CouponGenerationFormTestCase(TestCase):
     def test_form(self):
         form_data = {'quantity': 23, 'value': 42, 'type': 'monetary'}
         form = CouponGenerationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+class CouponFormTestCase(TestCase):
+    def setUp(self):
+        self.coupon = Coupon.objects.create_coupon('monetary', 100)
+
+    def test_wrong_code(self):
+        form_data = {'code': 'foo'}
+        form = CouponForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_right_code(self):
+        form_data = {'code': self.coupon.code}
+        form = CouponForm(data=form_data)
         self.assertTrue(form.is_valid())
 
