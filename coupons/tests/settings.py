6c46c3dffc2fd1422feb3da15983270de394bf73
akuryou <akuryou@byteweaver.net>
2014-11-13 11:05:43 +0100

    add test settings

---
+++ coupons/tests/settings.py
@@ -0,0 +1,75 @@
+"""Django settings for tests."""
+
+import os
+import django
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+
+SECRET_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+INTERNAL_IPS = ['127.0.0.1']
+
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = (
+    ('en', 'English'),
+)
+
+USE_TZ = True
+
+EXTERNAL_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+INTERNAL_APPS = [
+    'coupons',
+]
+
+INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
+
+MEDIA_URL = '/media/'   # Avoids https://code.djangoproject.com/ticket/21451
+
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'tests.urls'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'tests', 'static')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tests', 'additional_static'),
+    ("prefix", os.path.join(BASE_DIR, 'tests', 'additional_static')),
+]
+
+# Cache and database
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+if django.VERSION[:2] < (1, 6):
+    TEST_RUNNER = 'discover_runner.DiscoverRunner'
