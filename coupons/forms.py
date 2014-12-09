6af076f9a08a21a946fd1941b66b2119a12c2d05
Ben Murden <got.ben@gmail.com>
2014-12-09 20:03:37 +0900

    Small validation message typo.

--- coupons/forms.py
+++ coupons/forms.py
@@ -37,5 +37,5 @@ class CouponForm(forms.Form):
         if coupon.user is not None and coupon.user != self.user:
             raise forms.ValidationError(_("This code is not valid for your account."))
         if self.types is not None and coupon.type not in self.types:
-            raise forms.ValidationError(_("This code is not ment to be used here."))
+            raise forms.ValidationError(_("This code is not meant to be used here."))
         return code
