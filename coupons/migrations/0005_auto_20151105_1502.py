1a03249020a38c43e7cd92a766cead5ad2cf677d
Richard <richard@devisio.net>
2015-11-05 16:06:14 +0100

    implement data migration to create coupon user for each redeemed or coupon with user

--- coupons/migrations/0005_auto_20151105_1502.py
+++ coupons/migrations/0005_auto_20151105_1502.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import migrations
+
+
+def migrate_user_coupons(apps, schema_editor):
+    Coupon = apps.get_model('coupons', 'Coupon')
+    UserCoupon = apps.get_model('coupons', 'UserCoupon')
+    for coupon in Coupon.objects.all():
+        if coupon.user is not None or coupon.redeemed_at is not None:
+            UserCoupon.objects.create(
+                coupon=coupon,
+                user=coupon.user,
+                redeemed_at=coupon.redeemed_at
+            )
 
 
 class Migration(migrations.Migration):
@@ -11,4 +23,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(migrate_user_coupons),
     ]
