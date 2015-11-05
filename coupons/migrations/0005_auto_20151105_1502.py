a9c5c1243c8251b38384e6d63f64080b9e8ee79d
Richard <richard@devisio.net>
2015-11-05 16:03:03 +0100

    add empty migration as data migration

---
+++ coupons/migrations/0005_auto_20151105_1502.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('coupons', '0004_auto_20151105_1456'),
+    ]
+
+    operations = [
+    ]
