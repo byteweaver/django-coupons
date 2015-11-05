7b500b438a447e453a67a38f4f80c92d9856101e
Richard <richard@devisio.net>
2015-11-05 15:27:13 +0100

    test redeem without user

--- coupons/tests/test_use_cases.py
+++ coupons/tests/test_use_cases.py
@@ -11,7 +11,6 @@ class DefaultCouponTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create(username="user1")
         self.coupon = Coupon.objects.create_coupon('monetary', 100)
-        self.form = CouponForm(data={'code': self.coupon.code}, user=self.user)
 
     def test_redeem(self):
         self.coupon.redeem(self.user)
@@ -21,8 +20,9 @@ class DefaultCouponTestCase(TestCase):
         self.assertEquals(self.coupon.users.first().user, self.user)
 
     def test_redeem_via_form(self):
+        form = CouponForm(data={'code': self.coupon.code}, user=self.user)
         # form should be valid
-        self.assertTrue(self.form.is_valid())
+        self.assertTrue(form.is_valid())
         # perform redeem
         self.coupon.redeem(self.user)
         # coupon should be redeemed properly now
@@ -31,4 +31,18 @@ class DefaultCouponTestCase(TestCase):
         self.assertIsInstance(self.coupon.users.first().redeemed_at, datetime)
         self.assertEquals(self.coupon.users.first().user, self.user)
         # form should be invalid after redeem
-        self.assertTrue(self.form.is_valid())
+        self.assertTrue(form.is_valid())
+
+    def test_redeem_via_form_without_user(self):
+        form = CouponForm(data={'code': self.coupon.code})
+        # form should be valid
+        self.assertTrue(form.is_valid())
+        # perform redeem
+        self.coupon.redeem()
+        # coupon should be redeemed properly now
+        self.assertTrue(self.coupon.is_redeemed)
+        self.assertEquals(self.coupon.users.count(), 1)
+        self.assertIsInstance(self.coupon.users.first().redeemed_at, datetime)
+        self.assertIsNone(self.coupon.users.first().user)
+        # form should be invalid after redeem
+        self.assertTrue(form.is_valid())
