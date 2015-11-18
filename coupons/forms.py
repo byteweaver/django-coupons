3023ab41dd4eb5633266c359ae48387734fae9a6
Richard <hello@noxan.net>
2015-11-18 17:19:07 +0100

    fix issue with unbound coupon which has a couponuser with nulled user attached

--- coupons/forms.py
+++ coupons/forms.py
@@ -58,7 +58,7 @@ class CouponForm(forms.Form):
         except CouponUser.DoesNotExist:
             if coupon.user_limit is not 0:  # zero means no limit of user count
                 # only user bound coupons left and you don't have one
-                if coupon.user_limit is coupon.users.count():
+                if coupon.user_limit is coupon.users.filter(user__isnull=False).count():
                     raise forms.ValidationError(_("This code is not valid for your account."))
                 if coupon.user_limit is coupon.users.filter(redeemed_at__isnull=True).count():  # all coupons redeemed
                     raise forms.ValidationError(_("This code has already been used."))
