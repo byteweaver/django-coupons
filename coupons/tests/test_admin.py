9a866992b8cbeb269bea10fdb53f622637e8c2ed
Richard <richard@stromer.org>
2026-08-26 16:25:01 +0200

    test: cover merged admin contributions

--- coupons/tests/test_admin.py
+++ coupons/tests/test_admin.py
@@ -1,9 +1,10 @@
 from django.contrib.admin.sites import AdminSite
+from django.template.loader import get_template
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
-from coupons.admin import CouponAdmin
-from coupons.models import Coupon
+from coupons.admin import CampaignAdmin, CouponAdmin
+from coupons.models import Campaign, Coupon
 
 
 class CouponAdminTestCase(TestCase):
@@ -21,3 +22,15 @@ class CouponAdminTestCase(TestCase):
 
     def test_generate_coupons_url_is_registered(self):
         self.assertEqual(reverse("admin:generate_coupons"), "/admin/coupons/coupon/generate-coupons/")
+
+    def test_coupon_change_list_links_to_coupon_generation(self):
+        template = get_template("admin/coupons/coupon/change_list.html")
+
+        self.assertIn("Generate coupons", template.template.source)
+
+    def test_campaign_unused_count(self):
+        campaign = Campaign.objects.create(name="Launch")
+        Coupon.objects.create_coupon("monetary", 100, campaign=campaign)
+        campaign_admin = CampaignAdmin(Campaign, self.site)
+
+        self.assertEqual(campaign_admin.num_coupons_unused(campaign), 1)
